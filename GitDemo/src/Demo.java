a8448ff82fe92f6f951ca2df068f539a35a73bb9
SamruddhiKhadse <110394475+SamruddhiKhadse@users.noreply.github.com>
2022-08-01 17:37:46 +0530

    By Subodh

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -1,12 +1,13 @@
 
 public class Demo 
 {
+	public void m1()
+	{
+	}
+
 	public static void main(String[] args) 
 	{
-		public void m1()
-		{
-			System.out.println("Samruddhi");
-		}
+		
 		System.out.println("Git12");
 		
 	}
