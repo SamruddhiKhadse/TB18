a0d296097cc81756773b638dad3d0c0c8e69b879
Vaishanavi887 <110394513+Vaishanavi887@users.noreply.github.com>
2022-08-01 18:04:50 +0530

    By Vaishnavi

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -3,12 +3,17 @@ public class Demo
 {
 	public void m1()
 	{
+		
+	}
+	public void m2()
+	{
 	}
 
 	public static void main(String[] args) 
 	{
-		
+		System.out.println("Vaishnavi");
 		System.out.println("Git12");
+		System.out.println("Samruddhi");
 		
 	}
 
