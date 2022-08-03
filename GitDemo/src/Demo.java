15f58dfe5ccf1558f162c6c5108d6067986cbd60
Vaishanavi887 <110394513+Vaishanavi887@users.noreply.github.com>
2022-08-03 13:44:05 +0530

    By SUVI
    Hi xyz,
    
    two methods removed.

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -5,25 +5,14 @@ public class Demo
 	{
 		System.out.println("TB");
 	}
-	public void m2()
-	{
-		System.out.println("TB16");
-	}
-	public void m3()
-	{
-		System.out.println("TB18");
-	}
-	public void m4()
-	{
-		System.out.println("TB19");
-	}
+	
 	
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Vaishu");
 		System.out.println("Git12");
-		System.out.println("subya");
+		System.out.println("suvi");
 		
 	}
 	
